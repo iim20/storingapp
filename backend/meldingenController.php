6ae3fa31c55613dc974c36b45267c18bb237a43e
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-03-09 18:43:39 +0100

    Update INSERT-query
    Form is nu compleet.

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -3,22 +3,36 @@
 //Variabelen vullen
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
+$type = $_POST['type'];
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+} 
+else
+{
+    $prioriteit = false;
+}
 $melder = $_POST['melder'];
+$overig = $_POST['overig'];
 
-// echo $attractie . " / " . $capaciteit . " / " . $melder;
+// echo $attractie . " / " . $capaciteit . " / " . $overig . " / " . $melder ;
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, capaciteit, melder) VALUES(:attractie, :capaciteit, :melder)";
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) 
+VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 //3. Prepare
 $statement = $conn->prepare($query);
 //4. Execute
 $statement->execute([
     ":attractie" => $attractie,
+    ":type" => $type,
     ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
     ":melder" => $melder,
+    ":overige_info" => $overig
 ]);
 
 header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
--- meldingen/create.php
+++ meldingen/create.php
@@ -22,14 +22,37 @@
             <div class="form-group">
                 <label for="type">Type</label>
                 <!-- hier komt een dropdown -->
+                <select name="type" id="type" class="form-input">
+                    <option value=""> - Kies je klas - </option>
+                    <option value="achtbaan">Achtbaan</option>
+                    <option value="draaiend">Draaiende attractie</option>
+                    <option value="kinder">Kinder attractie</option>
+                    <option value="horeca">Restaurant</option>
+                    <option value="show">Shows</option>
+                    <option value="water">Water</option>
+                    <option value="overig">Overig</option>
+                </select>
             </div>
+
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
+
+            <div class="form-group">
+                <label for="newsletter">Prio:</label>
+                <input type="checkbox" name="prioriteit" id="newsletter">
+                <label for="newsletter">Melding met prioriteit</label>
+            </div>
+
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+
+            <div class="form-group">
+                <label for="overige_info">Overige info:</label>    
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
             
             <input type="submit" value="Verstuur melding">

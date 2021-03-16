d9a9b68e2cd70e53757632a4ae68349a4d54353b
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-03-16 09:40:39 +0100

    Oefenen inline php

---
+++ inline.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De krant</title>
+</head>
+<body>
+    <?php
+    $title = "Brandweer redt kat uit boom";
+    $date = "27 november 1997";
+    $contents = "(tyle lorem en druk op <tab>)";
+    $author = "Ilyas Mohamed";
+    ?>
+    <h1><?php echo  $title; ?></h1>
+    <p><em>Datum: <?php echo $date; ?></em></p>
+    <p><em>Author: <?php echo $author; ?></em></p>
+    <p><em>Content: <?php echo $contents; ?></em></p>
+    
+</body>
+</html>

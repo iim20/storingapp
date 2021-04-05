768b1189fff1212ad3c1aa4734246bd16a956cce
Ilyas I. Mohamed <d206947@edu.rocwb.nl>
2021-04-05 17:02:38 +0200

    Login.php aangemaakt
    msg komt te zien in url en boven de form. Form aangemaakt en gekoppeld aan loginController.

---
+++ login.php
@@ -0,0 +1,54 @@
+<?php 
+require_once 'backend/config.php';
+session_start();
+if(isset($_SESSION['user_id']))
+{
+    $msg = "Je bent al ingelogd!";
+    header("Location: $base_url/index.php?msg=$msg");    
+    exit();
+}
+?>
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>StoringApp</title>
+	<meta charset="utf-8">
+	<meta name="description" content="StoringApp voor technische dienst van DeveloperLand">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
+	<link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+</head>
+
+<body>
+
+    <?php require_once 'header.php'; ?>
+    <div class="container home">
+        <?php 
+        if(isset($_GET['msg'])) 
+        { 
+            echo "<div class='msg'>" . $_GET['msg'] . "</div>"; 
+        } 
+        ?>
+        <div class="form-group">
+            <form action="backend/loginController.php" method="POST">
+                <div class="form-group">
+                    <label for="username">Gebruikersnaam:</label>
+                    <input type="text" name="username" placeholder="user">
+                </div>
+                <div class="form-group">
+                    <label for="password">Wachtwoord:</label>
+                    <input type="password" name="password" placeholder="pass">
+                </div>
+                <div class="form-group">
+                    <label for=""></label>
+                    <input type="submit" value="login" class="form-group">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
